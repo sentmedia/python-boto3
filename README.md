f6cf693ee7cce1cc23247a6496edbb71167159cc
sentmedia <110376838+sentmedia@users.noreply.github.com>
2025-05-26 16:43:58 -0400

    adds docker-compose for localstack

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-boto3
 Repo for the Udemy course: Python Programming for AWS
+
+To start localstack docker container:
+    1. start docker desktop
+    2. cd to python-bot3/boto3
+    3. run docker compose up -d
--- boto3/setup.py
+++ boto3/setup.py
@@ -1,6 +1,12 @@
 import boto3
 
-s3 = boto3.resouce('s3')
+s3 = boto3.resource(
+    's3',
+    endpoint_url='http://localhost:4566',
+    aws_access_key_id='test',
+    aws_secret_access_key='test',
+    region_name='us-east-1'
+)
 
 for bucket in s3.buckets.all():
     print(bucket.name)
