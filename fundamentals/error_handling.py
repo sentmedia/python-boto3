3bc86d983d1080991cd3f631f0a85549504edc91
sentmedia <110376838+sentmedia@users.noreply.github.com>
2025-05-26 15:43:27 -0400

    adds error handling

--- fundamentals/error_handling.py
+++ fundamentals/error_handling.py
@@ -1,14 +1,17 @@
 def main():
-    service = 'Lambda'
+    service = 'EKS'
 
     service_status = get_service_status(service)
 
-    print(f"\n{service} service status: '{service_status}'")
-
-    if service_status == 'Operational':
-        print(f"Running operation on '{service}'")
-    else:
-        print(f"'{service}' is not operational")
+    if service_status:
+        print(f"\n{service} service status: '{service_status}'")
+    
+        if service_status == 'Operational':
+            print(f"Running operation on '{service}'")
+        else:
+            print(f"'{service}' is not operational")
+    else: 
+        print(f"\nService status for {service} could not be retrieved.")
 
 def get_service_status(service_name):
     aws_services_status = {
@@ -18,7 +21,11 @@ def get_service_status(service_name):
         "DynamoDB" : "Operational",
         "RDS" : "Operational"
     }
-    return aws_services_status[service_name]
+    try:
+        return aws_services_status[service_name]
+    except KeyError as ke:
+        print(f"ERROR: {ke}. Status for service {service_name} is not in our records.")
+        return None
 
 if __name__ == '__main__':
     main()
