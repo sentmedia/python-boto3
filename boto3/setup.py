0c23ae56baa0f4395403f54649313ee9638597d8
sentmedia <110376838+sentmedia@users.noreply.github.com>
2025-06-02 17:57:19 -0400

    adds comments to setup.py

--- boto3/setup.py
+++ boto3/setup.py
@@ -1,5 +1,6 @@
 import boto3
 
+# Instantiate the boto3 s3 resource with localstack
 s3 = boto3.resource(
     's3',
     endpoint_url='http://localhost:4566',
@@ -8,5 +9,6 @@ s3 = boto3.resource(
     region_name='us-east-1'
 )
 
+# List all buckets
 for bucket in s3.buckets.all():
     print(bucket.name)
